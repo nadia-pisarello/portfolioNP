1352325ef4db797fbe5c3d6786018da4cb90a7c6
nadiaPisarello <pisarello.nads@gmail.com>
2022-07-17 15:27:15 -0300

    reestructurando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoApComponent } from './components/logo-ap/logo-ap.component';
-import { BannerComponent } from './components/banner/banner.component';
 import { MediaComponent } from './components/media/media.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { EducationComponent } from './components/education/education.component';
 import { WorkXpComponent } from './components/work-xp/work-xp.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ProyectComponent } from './components/proyect/proyect.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoApComponent,
-    BannerComponent,
     MediaComponent,
     SkillsComponent,
     EducationComponent,
     WorkXpComponent,
     AboutMeComponent,
-    ProyectComponent
+    ProyectComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BannerComponent } from './banner.component';
+import { FooterComponent } from './footer.component';
 
-describe('BannerComponent', () => {
-  let component: BannerComponent;
-  let fixture: ComponentFixture<BannerComponent>;
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BannerComponent ]
+      declarations: [ FooterComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(BannerComponent);
+    fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
